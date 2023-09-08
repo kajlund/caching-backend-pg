d36aa566fb03a96a72bc92f00fb5fa83c0f2d8f5
Kaj Lund <kaj.lund@gmail.com>
2023-09-08 13:44:24 +0300

    Adds handlers for uncaught exceptions or -promise rejections

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from 'http'
+import util from 'util'
 
 import dotenv from 'dotenv'
 
@@ -6,6 +7,15 @@ dotenv.config()
 
 import log from './utils/log.js'
 import app from './app.js'
+
+process.on('uncaughtException', (err) => {
+  log.fatal(`UNCAUGHT EXCEPTION - ${err.stack || err}`)
+  throw err
+})
+
+process.on('unhandledRejection', (reason, p) => {
+  log.fatal(`UNHANDLED PROMISE REJECTION: ${util.inspect(p)} reason: ${reason}`)
+})
 
 log.info('Starting server')
 http.createServer(app).listen(process.env.PORT, () => {
